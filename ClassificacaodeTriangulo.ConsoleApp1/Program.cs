b5c55f7fa97d3ed636bcd735d7782f59e9666594
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-19 10:44:10 -0300

    Alteração

--- ClassificacaodeTriangulo.ConsoleApp1/Program.cs
+++ ClassificacaodeTriangulo.ConsoleApp1/Program.cs
@@ -6,18 +6,38 @@ internal class Program
     {
         while (true)
         {
+
+            // inicio 
             Console.Clear();
             Console.WriteLine("__________________________");
             Console.WriteLine("Classificação do Triangulo");
             Console.WriteLine("__________________________");
 
-            Console.WriteLine("Deseja continuar? (S?N)");
+
+            // logica
+            Console.WriteLine("Informe o lado de X:");
+            int ladox Convert.ToInt32(Console.ReadLine(ladox));
+
+            Console.WriteLine("Informe o lado de Y:");
+            int ladoy Convert.ToInt32(Console.ReadLine(ladoy));
+
+            Console.WriteLine("Informe o lado de Z:");
+            int ladoz Convert.TonInt32(Console.ReadLine(ladoz));
+
+            // interacao do usuario ( retorno externo )
+
+
+            Console.Write("Valor de X: " + ladox);
+            Console.Write("Valor de Y: " + ladoy);
+            Console.Write("Valor de Z: " + ladoz);
+
+
+            // final 
+            Console.Write("Deseja continuar? (S?N)");
             string opcaocontinuar = Console.ReadLine()!.ToUpper();
 
             if(opcaocontinuar!= "S")
             break;
-
-
 
 
         }
