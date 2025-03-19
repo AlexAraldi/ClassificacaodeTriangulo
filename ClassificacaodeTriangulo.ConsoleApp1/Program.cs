397c4fe6a3d43a78dba9c1b9d50f0dd886eb8e20
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-19 09:51:29 -0300

    Adição do cabeçalho

--- ClassificacaodeTriangulo.ConsoleApp1/Program.cs
+++ ClassificacaodeTriangulo.ConsoleApp1/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace ClassificacaodeTriangulo.ConsoleApp1
+﻿namespace ClassificacaodeTriangulo.ConsoleApp1;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        while (true)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Clear();
+            Console.WriteLine("__________________________");
+            Console.WriteLine("Classificação do Triangulo");
+            Console.WriteLine("__________________________");
+
+            Console.WriteLine("Deseja continuar? (S?N)");
+            string opcaocontinuar = Console.ReadLine()!.ToUpper();
+
+            if(opcaocontinuar!= "S")
+            break;
+
+
+
+
         }
+
+
+
+
     }
 }
