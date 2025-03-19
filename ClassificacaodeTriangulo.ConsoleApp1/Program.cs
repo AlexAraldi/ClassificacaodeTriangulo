0c42560325a76d04cd3c3220ab21ab7d34c4571d
AlexAraldi <alexanderaraldi@gmail.com>
2025-03-19 11:24:37 -0300

    Adição da verificação do Triangulo valido

--- ClassificacaodeTriangulo.ConsoleApp1/Program.cs
+++ ClassificacaodeTriangulo.ConsoleApp1/Program.cs
@@ -6,33 +6,39 @@ internal class Program
     {
         while (true)
         {
-
-            // inicio 
+            ///////// inicio _______________________________________________________________________
             Console.Clear();
             Console.WriteLine("__________________________");
             Console.WriteLine("Classificação do Triangulo");
             Console.WriteLine("__________________________");
 
-
-            // logica
-            Console.WriteLine("Informe o lado de X:");
+            ///////// logica________________________________________________________________________
+            Console.Write("Informe o lado de X: ");
             int ladox = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Informe o lado de Y:");
+            Console.Write("Informe o lado de Y: ");
             int ladoy = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Informe o lado de Z:");
+            Console.Write("Informe o lado de Z: ");
             int ladoz = Convert.ToInt32(Console.ReadLine());
 
-            // interacao do usuario ( retorno externo )
+            //////// interacao do usuario ( retorno externo )______________________________________
+            Console.WriteLine("Valor de X: "+ladox);
+            Console.WriteLine("Valor de Y: "+ladoy);
+            Console.WriteLine("Valor de Z: "+ladoz);
 
+            //////// verificacao do triangulo _____________________________________________________
+            bool medidasdotriangulovalidada =  
+            ladox + ladoy > ladoz &&
+            ladox + ladoz > ladoy &&
+            ladoz + ladoy > ladox;
 
-            Console.Write("Valor de X: " + ladox);
-            Console.Write("Valor de Y: " + ladoy);
-            Console.Write("Valor de Z: " + ladoz);
+            if (medidasdotriangulovalidada == true)
+            Console.WriteLine(" ** O Triangulo é Válido ** ");
+            else
+            Console.WriteLine(" >> Valores do Triangulo é inválida << ");
 
-
-            // final 
+            ///////// final ____________________________________________________________________ 
             Console.Write("Deseja continuar? (S?N)");
             string opcaocontinuar = Console.ReadLine()!.ToUpper();
 
